eec9e5351967af5ccdf5f25c55fabbe7d23d412f
Muhammed Farzin <muhammedfarzink5@gmail.com>
2023-06-01 20:00:01 +0530

    dummy data and widget constants created

---
+++ lib/presentation/constants/dummy_data.dart
@@ -0,0 +1,68 @@
+import 'package:cartify_hub/presentation/widgets/grid_view_listing_widget.dart';
+
+class DummyData {
+  static List<Product> topListingList = [
+    Product(
+      imageUrl: 'https://m.media-amazon.com/images/I/41-0KwfgelL.jpg',
+      title:
+          'Canon Powershot G10 14.7MP Digital Camera with 5X Wide Angle Optical Image Stabilized Zoom',
+      description:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+      price: 134000,
+      offerRate: 130000,
+      deliveryCharge: 49,
+    ),
+    Product(
+      imageUrl:
+          'https://img3.gadgetsnow.com/gd/images/products/additional/large/G390864_View_1/mobiles/smartphones/apple-iphone-14-pro-max-512-gb-deep-purple-6-gb-ram-.jpg',
+      title: 'Apple iPhone 14 Pro Max 512 GB (Deep Purple, 6 GB RAM)',
+      description:
+          "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+      price: 157999,
+      offerRate: 149999,
+      deliveryCharge: 0,
+    ),
+    Product(
+      imageUrl:
+          'https://image01.realme.net/general/20230512/1683873789325.png.webp?width=1440&height=1440&size=656747',
+      title: 'realme narzo N53',
+      description:
+          "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.",
+      price: 8999,
+      offerRate: 7999,
+      deliveryCharge: 49,
+    ),
+    Product(
+      imageUrl:
+          'https://image01.realme.net/general/20230511/1683779510389.jpg.webp?width=1080&height=1080&size=468380',
+      title: 'realme Smart TV Full HD 80cm 32',
+      description:
+          "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
+      price: 12999,
+      offerRate: 9999,
+      deliveryCharge: 0,
+    ),
+    Product(
+      imageUrl:
+          'https://m.media-amazon.com/images/I/41TiopG4SeL._SX300_SY300_QL70_FMwebp_.jpg',
+      title:
+          'boAt Xtend/Xtend RTL Smartwatch with Alexa Built-in, 1.69” HD Display, Multiple Watch Faces, Stress Monitor, Heart & SpO2 Monitoring, 14 Sports Modes, 5 ATM & 7 Days Battery(Sandy Cream)',
+      description:
+          "The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from \"de Finibus Bonorum et Malorum\" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.",
+      price: 7000,
+      offerRate: 2799,
+      deliveryCharge: 49,
+    ),
+    Product(
+      imageUrl:
+          'https://m.media-amazon.com/images/I/51YOeZehIKL._AC_UF1000,1000_QL80_.jpg',
+      title:
+          'boAt Airdopes 170 TWS Earbuds with 50H Playtime, Quad Mics ENx™ Tech, Low Latency Mode, 13mm Drivers, ASAP™ Charge, IPX4, IWP™, Touch Controls & BT v5.3(Classic Black)',
+      description:
+          "Phasellus ut magna sed lacus lobortis condimentum a vel lorem. Maecenas et mauris eu massa tristique commodo tempus vel libero. Vivamus eu erat eget massa porttitor sodales. Suspendisse a iaculis risus. Sed hendrerit, metus at viverra maximus, ante massa pretium diam, sodales euismod massa nisi id nibh. Fusce gravida mauris quam, quis hendrerit enim convallis consectetur. Praesent ac ultrices quam. Morbi a orci vitae nunc iaculis luctus suscipit ullamcorper mauris. Nulla nec nisl quis elit finibus egestas et eget justo. Cras scelerisque nisi vitae lectus accumsan accumsan. Vivamus nibh dolor, tincidunt et nibh at, imperdiet mattis metus. Sed id elit sem. Interdum et malesuada fames ac ante ipsum primis in faucibus. In sapien magna, tempor in commodo ac, luctus eget nunc. Cras semper elit fringilla molestie maximus. Suspendisse varius tellus vitae leo rhoncus varius.",
+      price: 4499,
+      offerRate: 1338,
+      deliveryCharge: 0,
+    ),
+  ];
+}
---
+++ lib/presentation/constants/widget_constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class WidgetConstants {
+  static const Widget height5 = SizedBox(
+    height: 5,
+  );
+}
