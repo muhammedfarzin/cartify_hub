c4f3b1c6496f7dacc721a3f55ca893ec78511ffc
Muhammed Farzin <muhammedfarzink5@gmail.com>
2023-06-03 19:42:09 +0530

    Top LIsting and Grid View added

--- lib/presentation/home/screen_home.dart
+++ lib/presentation/home/screen_home.dart
@@ -1,4 +1,6 @@
+import 'package:cartify_hub/presentation/constants/dummy_data.dart';
 import 'package:cartify_hub/presentation/main/widgets/offer_slider.dart';
+import 'package:cartify_hub/presentation/widgets/grid_view_listing_widget.dart';
 import 'package:flutter/material.dart';
 
 class ScreenHome extends StatelessWidget {
@@ -7,13 +9,35 @@ class ScreenHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView(
-      children: const [
+      children: [
         // Offer Slider
-        Padding(
+        const Padding(
           padding: EdgeInsets.symmetric(vertical: 10.0),
           child: OfferSlider(),
         ),
         // End of Offer Slider
+
+        // Top Listing
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Top Listing",
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+
+              // Grid View
+              GridViewListingWidget(
+                items: DummyData.topListingList,
+                rowCount: 2,
+              )
+              // End of Grid View
+            ],
+          ),
+        ),
+        // End of Top Listing
       ],
     );
   }
---
+++ lib/presentation/widgets/grid_view_listing_widget.dart
@@ -0,0 +1,144 @@
+import 'package:cartify_hub/functions/currency_format.dart';
+import 'package:cartify_hub/presentation/constants/widget_constants.dart';
+import 'package:flutter/material.dart';
+
+class GridViewListingWidget extends StatelessWidget {
+  final List<Product> items;
+  final double width;
+  final double childAspectRatio;
+  final int? rowCount;
+  const GridViewListingWidget({
+    super.key,
+    required this.items,
+    this.width = 130,
+    this.childAspectRatio = 1 / 1.55,
+    this.rowCount,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    // bool showMore = false;
+    final screenWidth = MediaQuery.of(context).size.width;
+    final columnCount = screenWidth ~/ width;
+    final itemCount = columnCount * (rowCount ?? 2);
+    final selectedItemCount = (itemCount < items.length && rowCount != null)
+        ? itemCount
+        : items.length;
+
+    return GridView.builder(
+      itemCount: selectedItemCount,
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: columnCount,
+        childAspectRatio: childAspectRatio,
+      ),
+      itemBuilder: (context, index) {
+        final item = items[index];
+        // Checking Show more or not
+        if (selectedItemCount == index + 1 &&
+            itemCount < items.length &&
+            rowCount != null) {
+          return const Card(
+            child: Text("Show More"),
+          );
+        } else {
+          return LayoutBuilder(
+            builder: (context, constraints) {
+              final maxWidth = constraints.maxWidth;
+              return Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: Stack(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          // Product Image
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            child: AspectRatio(
+                              aspectRatio: 1,
+                              child: Container(
+                                color: Theme.of(context).colorScheme.background,
+                                padding: const EdgeInsets.all(5),
+                                child: Image(
+                                  image: NetworkImage(item.imageUrl),
+                                  width: double.infinity,
+                                ),
+                              ),
+                            ),
+                          ),
+                          // End of Product Image
+
+                          WidgetConstants.height5,
+
+                          // Product Details
+                          // Title
+                          Text(
+                            item.title,
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+
+                          // Price
+                          Text(
+                            CurrencyFormat.formatCurrency(item.offerRate),
+                            style: const TextStyle(
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          // End of Product Details
+                        ],
+                      ),
+
+                      // Discount Rate
+                      Positioned(
+                        top: maxWidth - 40,
+                        right: 0,
+                        child: Container(
+                          padding: const EdgeInsets.all(3),
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                            color: Theme.of(context).colorScheme.primary,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Text(
+                            "${CurrencyFormat.getPercentage(originalPrice: item.price, offerPrice: item.offerRate)}% off",
+                            style: const TextStyle(
+                              fontSize: 15,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                      // End of Discount Rate
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        }
+      },
+    );
+  }
+}
+
+class Product {
+  final String imageUrl;
+  final String title;
+  final String description;
+  final double price;
+  final double offerRate;
+  final double deliveryCharge;
+
+  Product({
+    required this.imageUrl,
+    required this.title,
+    required this.description,
+    required this.price,
+    required this.offerRate,
+    required this.deliveryCharge,
+  });
+}
