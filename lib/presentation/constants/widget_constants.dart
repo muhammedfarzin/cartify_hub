f5d7d341224500b13f09b421ef6117fcb025bfd8
Muhammed Farzin <muhammedfarzink5@gmail.com>
2023-06-05 17:40:16 +0530

    Line Height Added to Constants

--- lib/presentation/constants/widget_constants.dart
+++ lib/presentation/constants/widget_constants.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
 
 class WidgetConstants {
+// Size Constants
+  // Height Constants
   static const Widget height5 = SizedBox(
     height: 5,
   );
+
+  // Width Constants
+  static const Widget width10 = SizedBox(
+    width: 10,
+  );
+// End of Size Constants
+
+// Line Constants
+  static Widget lineHeight1 = Container(
+    color: Colors.black12,
+    height: 1,
+    width: double.infinity,
+  );
+// End of Line Constants
 }
