b95ec3ce23127a295e3ec9db7a8fb4794b56d8ad
Muhammed Farzin <muhammedfarzink5@gmail.com>
2023-07-07 22:28:57 +0530

    Dummy Data modified

--- lib/presentation/constants/dummy_data.dart
+++ lib/presentation/constants/dummy_data.dart
@@ -1,5 +1,3 @@
-import 'package:cartify_hub/presentation/widgets/grid_view_listing_widget.dart';
-
 class DummyData {
   static List<Product> topListingList = [
     Product(
@@ -13,24 +11,31 @@ class DummyData {
       deliveryCharge: 49,
     ),
     Product(
-      imageUrl:
-          'https://img3.gadgetsnow.com/gd/images/products/additional/large/G390864_View_1/mobiles/smartphones/apple-iphone-14-pro-max-512-gb-deep-purple-6-gb-ram-.jpg',
-      title: 'Apple iPhone 14 Pro Max 512 GB (Deep Purple, 6 GB RAM)',
-      description:
-          "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
-      price: 157999,
-      offerRate: 149999,
-      deliveryCharge: 0,
-    ),
+        imageUrl:
+            'https://img3.gadgetsnow.com/gd/images/products/additional/large/G390864_View_1/mobiles/smartphones/apple-iphone-14-pro-max-512-gb-deep-purple-6-gb-ram-.jpg',
+        title: 'Apple iPhone 14 Pro Max 512 GB (Deep Purple, 6 GB RAM)',
+        description:
+            "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+        price: 157999,
+        offerRate: 149999,
+        deliveryCharge: 0,
+        varient: {
+          "Size": "6GB+512GB",
+          "Color": "Deep Purple",
+        }),
     Product(
       imageUrl:
           'https://image01.realme.net/general/20230512/1683873789325.png.webp?width=1440&height=1440&size=656747',
       title: 'realme narzo N53',
       description:
           "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.",
-      price: 8999,
-      offerRate: 7999,
+      price: 10999,
+      offerRate: 8999,
       deliveryCharge: 49,
+      varient: {
+        "Size":"4GB+64GB",
+        "Color":"Feather Gold",
+      }
     ),
     Product(
       imageUrl:
@@ -60,7 +65,7 @@ class DummyData {
           'boAt Airdopes 170 TWS Earbuds with 50H Playtime, Quad Mics ENx™ Tech, Low Latency Mode, 13mm Drivers, ASAP™ Charge, IPX4, IWP™, Touch Controls & BT v5.3(Classic Black)',
       description:
           "Phasellus ut magna sed lacus lobortis condimentum a vel lorem. Maecenas et mauris eu massa tristique commodo tempus vel libero. Vivamus eu erat eget massa porttitor sodales. Suspendisse a iaculis risus. Sed hendrerit, metus at viverra maximus, ante massa pretium diam, sodales euismod massa nisi id nibh. Fusce gravida mauris quam, quis hendrerit enim convallis consectetur. Praesent ac ultrices quam. Morbi a orci vitae nunc iaculis luctus suscipit ullamcorper mauris. Nulla nec nisl quis elit finibus egestas et eget justo. Cras scelerisque nisi vitae lectus accumsan accumsan. Vivamus nibh dolor, tincidunt et nibh at, imperdiet mattis metus. Sed id elit sem. Interdum et malesuada fames ac ante ipsum primis in faucibus. In sapien magna, tempor in commodo ac, luctus eget nunc. Cras semper elit fringilla molestie maximus. Suspendisse varius tellus vitae leo rhoncus varius.",
-      price: 4499.659,
+      price: 4499.65,
       offerRate: 1338.76,
       deliveryCharge: 0,
     ),
@@ -76,4 +81,24 @@ class DummyData {
       deliveryCharge: 49,
     ),
   ];
+}
+
+class Product {
+  final String imageUrl;
+  final String title;
+  final String description;
+  final double price;
+  final double offerRate;
+  final double deliveryCharge;
+  final Map<String, dynamic>? varient;
+
+  Product({
+    required this.imageUrl,
+    required this.title,
+    required this.description,
+    required this.price,
+    required this.offerRate,
+    required this.deliveryCharge,
+    this.varient,
+  });
 }
