68f19f48ab62411975651408fb8f9175de937613
Muhammed Farzin <muhammedfarzink5@gmail.com>
2023-07-19 20:43:36 +0530

    ShippingFeeWidget Modified

--- lib/presentation/widgets/shipping_fee_widget.dart
+++ lib/presentation/widgets/shipping_fee_widget.dart
@@ -13,21 +13,23 @@ class ShippingFeeWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     final ColorScheme colorScheme = Theme.of(context).colorScheme;
 
-    return Row(
-      children: [
-        Icon(
-          Icons.local_shipping,
-          color: colorScheme.primary,
-          size: 20,
-        ),
-        WidgetConstants.width5,
-        Text(
-          deliveryCharge == 0
-              ? "Free Shipping"
-              : "+${deliveryCharge.floor()} Shipping Fee",
-          style: TextStyle(color: colorScheme.primary),
-        ),
-      ],
-    );
+    if (deliveryCharge == 0) {
+      return Row(
+        children: [
+          Icon(
+            Icons.local_shipping,
+            color: colorScheme.primary,
+            size: 20,
+          ),
+          WidgetConstants.width5,
+          Text(
+            "Free Shipping",
+            style: TextStyle(color: colorScheme.primary),
+          ),
+        ],
+      );
+    } else {
+      return Text("+${deliveryCharge.floor()} Shipping Fee");
+    }
   }
 }
